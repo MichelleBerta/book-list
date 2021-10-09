f96b4a99a5d724ccadc2e03087f613d6cca96af5
Michelle Berta <bertafamily@sbcglobal.net>
2021-10-09 16:06:37 -0500

    finish setting up files

---
+++ script.js
@@ -0,0 +1,35 @@
+// Book Class:  represents a Book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class:  Handle UI Tasks
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+        {
+            title: 'Book One',
+            author: 'John Doe',
+            isbn: '3434434'
+        },
+        {
+            title: 'Book Two',
+            author: 'Jane Doe',
+            isbn: '45545'
+        }
+    ];
+    }
+}
+
+// Store Class:  Handles storage
+
+
+// Event:  Display books
+
+
+// Event:  Add a book
+// Event:  Remove a book
